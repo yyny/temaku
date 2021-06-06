d5a921436e5f5702bbc8204a0bcecd0309fd68a6
yyny <6576327+yyny@users.noreply.github.com>
2021-06-07 00:35:33 +0200

    add libc file writer

--- example.c
+++ example.c
@@ -8,6 +8,7 @@ const char usage[] =
 "=USAGE\n"
 "  _progname_ |--help|               You're looking at it!\n"
 "  _progname_ |--version|            Print %F{blue}version%f and %F{BLUE}stuff%f\n"
+"  _progname_ |--export-usage|       Showcase custom writers\n"
 "  _progname_ |<file>|               Do the thing with the file\n"
 "=OPTIONS\n"
 "  |-florg|                        Enable the |florg| capability\n"
@@ -23,6 +24,11 @@ int main(int argc, char **argv)
 {
     if (argc < 2 || strcmp(argv[1], "--help") == 0) {
         temaku_markup(NULL, &temaku_stderr_writer, usage, 0);
+    } else if (strcmp(argv[1], "--export-usage") == 0) {
+        temaku_file_writer_t writer = temaku_file_writer_open("usage.txt", "wb");
+        temaku_options_t options = TEMAKU_DEFAULT_OPTIONS;
+        options.do_markup = false;
+        temaku_markup(&options, &writer.writer, usage, 0);
     } else {
         temaku_options_t options = TEMAKU_DEFAULT_OPTIONS;
         options.do_color = false;
--- include/temaku_libc.h
+++ include/temaku_libc.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+typedef struct temaku_file_writer temaku_file_writer_t;
+
+struct temaku_file_writer {
+    temaku_writer_t writer;
+    FILE *fp;
+};
+
+static int temaku_file_writer_cb(TEMAKU_SELF *self, const void *data, size_t size)
+{
+    temaku_file_writer_t *writer = (temaku_file_writer_t *)self;
+    return fwrite(data, sizeof(char), size, writer->fp);
+}
 static int temaku_stdout_writer_cb(TEMAKU_SELF *self, const void *data, size_t size)
 {
     return fwrite(data, sizeof(char), size, stdout);
@@ -14,5 +26,17 @@ static int temaku_stderr_writer_cb(TEMAKU_SELF *self, const void *data, size_t s
 
 temaku_writer_t temaku_stdout_writer = temaku_stdout_writer_cb;
 temaku_writer_t temaku_stderr_writer = temaku_stderr_writer_cb;
+
+static temaku_file_writer_t temaku_file_writer_new(FILE *fp)
+{
+    temaku_file_writer_t writer;
+    writer.writer = temaku_file_writer_cb;
+    writer.fp = fp;
+    return writer;
+}
+static temaku_file_writer_t temaku_file_writer_open(const char *path, const char *mode)
+{
+    return temaku_file_writer_new(fopen(path, mode));
+}
 
 #endif /* TEMAKU_LIBC_H */
